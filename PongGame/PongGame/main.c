d2d3a39a3239c0596df62b6aa76e80fef795fadc
Bjørn Olav Loftsgarden <bjornolo@ntnu.no>
2022-09-01 12:31:01 +0200

    First commit

---
+++ PongGame/PongGame/main.c
@@ -0,0 +1,18 @@
+/*
+ * PongGame.c
+ *
+ * Created: 01.09.2022 12:22:03
+ * Author : bjornolo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
