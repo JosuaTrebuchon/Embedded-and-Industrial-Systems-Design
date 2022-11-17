a84bb5b5e2d5c660209d62c760c59c3dbcc14e3b
bjornolo <36789895+bjornolo@users.noreply.github.com>
2022-11-17 21:56:33 +0100

    cleanup BOL

--- PongGame/PongGame/ATmega162_driver.h
+++ PongGame/PongGame/ATmega162_driver.h
@@ -2,8 +2,7 @@
 #include <avr/io.h>
 #include <stdio.h>
 
-// #define F_CPU 4915200
-#define FOSC 4915200 // 1843200
+#define FOSC 4915200 
 #define BAUD 9600
 #define UBRR FOSC / 16 / BAUD - 1
 #define BASE_ADDRESS 0x0000
